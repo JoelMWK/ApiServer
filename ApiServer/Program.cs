b6d0b4b3180173843304bbee01b677351ed408ee
Joel Kustemo <kustemo.joel@elev.ga.ntig.se>
2023-02-15 13:53:06 +0100

    commi

--- ApiServer/Program.cs
+++ ApiServer/Program.cs
@@ -3,15 +3,25 @@ WebApplication app = WebApplication.Create();
 app.Urls.Add("http://localhost:3000");
 app.Urls.Add("http://*:3000");
 
-Superhero hero = new Superhero();
-hero.Name = "Shrek";
-hero.Power = 900;
-hero.Sexy = true;
+List<Superhero> heroes = new();
+heroes.Add(new() { Name = "Shrek", Power = 99999999 });
+heroes.Add(new() { Name = "Donkey", Power = 999999 });
+heroes.Add(new() { Name = "Pickles", Power = 999999999 });
 
 app.MapGet("/", Answer);
-app.MapGet("/superhero/", () => { return hero; });
+app.MapGet("/superhero/", () => { return heroes; });
+app.MapGet("/superhero/{num}", (int num) => { if (num >= 0 && num < heroes.Count) return Results.Ok(heroes[num]); return Results.NotFound(); });
 
 
+app.MapPost("/superhero/", (Superhero h) =>
+{
+    if (heroes.Find(x => x.Name == h.Name) != null)
+    {
+        return Results.BadRequest();
+    }
+    heroes.Add(h);
+    return Results.Ok();
+});
 app.Run();
 
 
--- ApiServer/Superhero.cs
+++ ApiServer/Superhero.cs
@@ -4,5 +4,4 @@ public class Superhero
 {
     public string Name { get; set; }
     public int Power { get; set; }
-    public bool Sexy { get; set; }
 }
